6e748da125a94071070cc69910c53c1223909920
rolud <rolud.dev@gmail.com>
2025-11-19 14:49:25 +0100

    Implement report handling functions in app-store-connect-reports.js
    - Added functions to read report instances and segments from the App Store Connect API.
    - Created handleReadReports and handleRequestReports functions to streamline report retrieval and selection process.
    - Refactored main function to utilize new handling functions for improved readability and maintainability.

--- app-store-connect-reports.js
+++ app-store-connect-reports.js
@@ -75,6 +75,28 @@ const readReportInformations = async (token, id) => {
     return data;
 }
 
+const readReportInstance = async (token, id) => {
+    const response = await fetch(`${APP_STORE_CONNECT_API_URL}/analyticsReportInstances/${id}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
+const readReportSegment = async (token, id) => {
+    const response = await fetch(`${APP_STORE_CONNECT_API_URL}/analyticsReportInstances/${id}/segments`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
 
 const requestReports = async (token, type) => {
     const response = await fetch(`${APP_STORE_CONNECT_API_URL}/analyticsReportRequests`, {
@@ -104,6 +126,52 @@ const requestReports = async (token, type) => {
     return data;
 }
 
+const handleReadReports = async (token) => {
+    const reports = await readReportRequests(token);
+    let choices =reports.data.map(it => ({name: `${it.attributes.accessType} - ${it.id}`, value: it.id} ))
+
+    let id = await select({
+        message: 'Select report to read',
+        choices
+    });
+    const reportRequestDetails = await readReportRequestDetails(token, id);
+    choices = reportRequestDetails.data.map(it => ({name: `${it.attributes.name} (${it.attributes.category}) - ${it.id}`, value: it.id} ))
+    id = await select({
+        message: 'Select report to read',
+        choices
+    });
+    const reportName = choices.find(it => it.value === id).name;
+    const reportInformation = await readReportInformations(token, id);
+
+    choices = reportInformation.data.map(it => ({name: `${it.attributes.granularity} - ${it.attributes.processingDate}`, value: it.id}))
+
+    id = await select({
+        message: `${reportName} - Select instance to read`,
+        choices
+    })
+
+    const reportSegments = await readReportSegment(token, id);
+    console.log(`Download report clicking on the link`)
+    console.log('\n\n');
+    for (const segment of reportSegments.data) {
+        console.log(`⬇️⬇️ ${segment.attributes.sizeInBytes} B ⬇️⬇️`);
+        console.log(`${segment.attributes.url}`);
+        console.log('\n\n');
+    }
+}
+
+const handleRequestReports = async (token) => {
+    const type = await select({
+        message: 'Select report type',
+        choices: [
+            { name: 'Ongoing', value: 'ONGOING' },
+            { name: 'Daily', value: 'DAILY' },
+        ]
+    })
+    const res = await requestReports(token, type);
+    console.log(`Requested ${type} report, id: ${res.data.id}`);
+}
+
 const main = async () => {
     const token = await createToken();
 
@@ -115,34 +183,10 @@ const main = async () => {
         ]
     });
     
-    if (answer === 'readReportRequests') {
-        const reports = await readReportRequests(token);
-        let choices =reports.data.map(it => ({name: `${it.attributes.accessType} - ${it.id}`, value: it.id} ))
-
-        let id = await select({
-            message: 'Select report to read',
-            choices
-        });
-        const reportRequestDetails = await readReportRequestDetails(token, id);
-        choices = reportRequestDetails.data.map(it => ({name: `${it.attributes.name} (${it.attributes.category}) - ${it.id}`, value: it.id} ))
-        id = await select({
-            message: 'Select report to read',
-            choices
-        });
-        const reportInformation = await readReportInformations(token, id);
-        console.log(JSON.stringify(reportInformation, null, 2));
-    } 
-    else if (answer === 'requestReports') {
-        const type = await select({
-            message: 'Select report type',
-            choices: [
-                { name: 'Ongoing', value: 'ONGOING' },
-                { name: 'Daily', value: 'DAILY' },
-            ]
-        })
-        const res = await requestReports(token, type);
-        console.log(`Requested ${type} report, id: ${res.data.id}`);
-    }
+    if (answer === 'readReportRequests') 
+        await handleReadReports(token);
+    else if (answer === 'requestReports') 
+        await handleRequestReports(token);
 }
 
 main();
